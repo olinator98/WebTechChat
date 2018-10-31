4664fd05790a9b927eacc8a2474218bd8469a9a3
Oliver Werlen <oliverwerlen@bluewin.ch>
2018-10-31 15:08:43 +0100

    Added some <br>

--- source/php/login.php
+++ source/php/login.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="de">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login Form</title>
-  </head>
-  <body>
+
     <form id = "loginForm" name = "loginForm" method = "POST" action = "checkLogin.php">
       Username:
-      <input type = "text" id = "username" name = "username" placeholder = "username"/>
+      <input type = "text" id = "username" name = "username" placeholder = "username" required/><br>
       Password:
-      <input type = "password" id = "password" name = "password" placeholder = "password"/>
+      <input type = "password" id = "password" name = "password" placeholder = "password" required/><br>
       <button type = "submit">Login </button>
     </form>
-  </body>
-</html>
--- source/php/register.php
+++ source/php/register.php
@@ -1,19 +1,19 @@
 
     <form id = "registerForm" name = "registerForm" method = "POST" action = "doRegistration.php">
       Email:
-      <input type = "email" id = "email" name = "email" placeholder = "Email" required/>
+      <input type = "email" id = "email" name = "email" placeholder = "Email" required/><br>
       Password:
-      <input type = "password" id = "passwordRegister" name = "passwordRegister" placeholder = "password" required/>
+      <input type = "password" id = "passwordRegister" name = "passwordRegister" placeholder = "password" required/><br>
       Confirm Password:
-      <input type = "password" id = "passwordConfirmRegister" name = "passwordConfirmRegister" placeholder = "password confirmation" onblur = "passwordEqualsPasswordConfirm()" required/>
+      <input type = "password" id = "passwordConfirmRegister" name = "passwordConfirmRegister" placeholder = "password confirmation" onblur = "passwordEqualsPasswordConfirm()" required/><br>
       Prename:
-      <input type = "text" id = "prename" name = "prename" placeholder = "Prename" required/>
+      <input type = "text" id = "prename" name = "prename" placeholder = "Prename" required/><br><br>
       Surname:
-      <input type = "text" id = "surname" name = "surname" placeholder = "Surname" required/>
+      <input type = "text" id = "surname" name = "surname" placeholder = "Surname" required/><br>
       Adress:
-      <input type = "text" id = "adress" name = "adress" placeholder = "Adress" required/>
+      <input type = "text" id = "adress" name = "adress" placeholder = "Adress" required/><br>
       Country:
-      <input type = "text" id = "country" name = "country" placeholder = "Country" required/>
+      <input type = "text" id = "country" name = "country" placeholder = "Country" required/><br>
 
 
       <button type = "submit">Register </button>
