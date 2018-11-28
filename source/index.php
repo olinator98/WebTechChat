e2fd8b6bed3b8dd133a6a94a0ae3c75f46f22274
Oliver Werlen <oliwerlen98@gmail.com>
2018-11-28 14:32:53 +0100

    Basic StartPage and ChatPage

--- source/index.php
+++ source/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="de">
   <head>
@@ -10,7 +11,7 @@
   <body>
 
         <header>
-
+            
             <nav class = "navigation" name = "navigation" id = "navigation">
                 <a href="index.php" class="active">Home</a>
                 <a href="#login">Login</a>
@@ -21,6 +22,7 @@
             </nav>
         </header>
 
+
         <article class = "login" name = "login" id = "login">
             <?php
                 include("php/login.php");
@@ -28,15 +30,13 @@
         </article>
 
         <article class = "information" name = "information" id = "information">
-
-
            <p>
-           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-           eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-           voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-           clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-           amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+           eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam 
+           voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet 
+           clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit 
+           amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
             Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
@@ -53,21 +53,15 @@
         }
         </script>
 
-
-        <script>
-        function toggleMenu() {
-            var x = document.getElementById("navigation");
-            if (x.className === "navigation") {
-                x.className += " responsive";
-            } else {
-                x.className = "navigation";
-            }
-        }
-        </script>
-
         <footer class = "footer">
             <p>Oliver Werlen | Sven Guggisberg</p>
             <p>Webtech</p>
         </footer>
+
+
+
+        
+
+
   </body>
 </html>
--- source/php/register.php
+++ source/php/register.php
@@ -1,23 +1,24 @@
-
-
-    <form id = "registerForm" name = "registerForm" method = "POST" action = "doRegistration.php">
-      Email:
-      <input type = "email" id = "email" name = "email" placeholder = "Email" required/><br>
-      Password:
-      <input type = "password" id = "passwordRegister" name = "passwordRegister" placeholder = "password" required/><br>
-      Confirm Password:
-      <input type = "password" id = "passwordConfirmRegister" name = "passwordConfirmRegister" placeholder = "password confirmation" onblur = "passwordEqualsPasswordConfirm()" required/><br>
-      Prename:
-      <input type = "text" id = "prename" name = "prename" placeholder = "Prename" required/><br><br>
-      Surname:
-      <input type = "text" id = "surname" name = "surname" placeholder = "Surname" required/><br>
-      Adress:
-      <input type = "text" id = "adress" name = "adress" placeholder = "Adress" required/><br>
-      Country:
+    <h1>
+        Register
+    </h1>
+    <form id = "registerForm" name = "registerForm" method = "POST" action = "ajaxChat.php">
+      <br>Email:<br>
+      <input type = "email" id = "email" name = "email" placeholder = "Email" required/>
+      <br>Password:<br>
+      <input type = "password" id = "passwordRegister" name = "passwordRegister" placeholder = "password" required/>
+      <br>Confirm Password:<br>
+      <input type = "password" id = "passwordConfirmRegister" name = "passwordConfirmRegister" placeholder = "password confirmation" onblur = "passwordEqualsPasswordConfirm()" required/>
+      <br>Prename:<br>
+      <input type = "text" id = "prename" name = "prename" placeholder = "Prename" required/>
+      <br>Surname:<br>
+      <input type = "text" id = "surname" name = "surname" placeholder = "Surname" required/>
+      <br>Adress:<br>
+      <input type = "text" id = "adress" name = "adress" placeholder = "Adress" required/>
+      <br>Country:<br>
       <input type = "text" id = "country" name = "country" placeholder = "Country" required/><br>
 
 
-      <button type = "submit">Register </button>
+      <input type = "submit"> </button>
     </form> 
 
 <script>
